534b7b554c845fe3e8250f63af64db75df61e63d
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-03-12 15:10:29 +0800

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Quetion1{
+    public static void main(String[]args){
+    	Scanner scan=new Scanner(System.in);
+    	 System.out.println("Please enter a message");
+    	 
+        String message =scan.nextLine();
+        System.out.println("Your message is: " + message);
+       
+    }
+}
