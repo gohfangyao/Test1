85096cfa56b5aeebd761c6c8e5b825914f3fb456
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-03-12 15:18:06 +0800

    Create question2.java

---
+++ question2.java
@@ -0,0 +1,18 @@
+public class Question2 {
+	    private static final boolean True = false;
+		static boolean value1=True;
+	    static double value2=2.56;
+	    static float value3=0.53f;
+	    static int value4=50;
+	    static long value5=652656;
+	    static String value6="Nice to meet you";
+	   public static void main(String[] args) {
+	      System.out.println("Default values are");
+	      System.out.println("Value1 = " + value1);
+	      System.out.println("Value2 = " + value2);
+	      System.out.println("Value3 = " + value3);
+	      System.out.println("Value4 = " + value4);
+	      System.out.println("Value5 = " + value5);
+	      System.out.println("Value6 = " + value6);
+	   }
+	}
